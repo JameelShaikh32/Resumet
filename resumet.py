ca1a39e87b65075e6a81b76c0beb1fc4948381eb
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2022-11-12 21:54:48 +0530

    Update resumet.py

--- resumet.py
+++ resumet.py
@@ -15,11 +15,7 @@ import pymysql
 from courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
 import pafy
 import plotly.express as px
-import spacy
-import nltk
 
-nltk.download()
-spacy.load('en_core_web_sm')
 
 def fetch_yt_video(link):
     video = pafy.new(link)
