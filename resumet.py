5468f2a62061d4a957e138fd236cd41a3cf76f99
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2022-11-13 17:16:48 +0530

    Update resumet.py

--- resumet.py
+++ resumet.py
@@ -84,7 +84,7 @@ def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level
 
 st.set_page_config(
    page_title="Resumet - Resume Analyzer",
-   page_icon='./Logo/resumet-favicon.png',
+   page_icon='./logo/resumet-favicon.png',
 )
 
 hide_st_style = """
@@ -101,7 +101,7 @@ def run():
     activities = ["Normal User", "Admin"]
     
     choice = st.sidebar.selectbox("Choose among the given options:", activities)
-    img = Image.open('./Logo/resumet-logo.png')
+    img = Image.open('./logo/resumet-logo.png')
     img = img.resize((150,150))
     st.image(img)
     st.title("Resume Analyser")
